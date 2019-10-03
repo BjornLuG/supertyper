7f451849cbbd7262745ab5afdf63a0528150f8e3
BjornLuG <bjornlu.dev@gmail.com>
2019-10-03 17:22:12 +0800

    Add main entry point

--- __main__.py
+++ __main__.py
@@ -1,5 +1,22 @@
 import pygame
+import constants
 
-pygame.init()
+def main():
+  # Initialize pygame
+  pygame.init()
 
-pygame.display.set_mode((500,500))
+  # Set title
+  pygame.display.set_caption(constants.APP_NAME)
+
+  # Create screen display
+  screen = pygame.display.set_mode((constants.APP_WIDTH, constants.APP_HEIGHT))
+  
+  running = True
+
+  while running:
+    for event in pygame.event.get():
+      if (event.type == pygame.QUIT):
+        running = False
+
+if __name__ == "__main__":
+  main()
---
+++ constants.py
@@ -0,0 +1,3 @@
+APP_NAME = "Supertyper"
+APP_WIDTH = 500
+APP_HEIGHT = 500
